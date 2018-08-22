e775ea3e4da40d9a0e5ac39dbe67af15be181e50
Jennifer Calhoon <jennifercalhoon@Jennifers-MacBook-Pro.local>
2018-08-21 17:42:24 -0700

    First and Final Commit

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title-container__title">Weather</h1>
+                <p className="title-container__subtitle">Find out your daily forecast</p>
+            </div>
+        );
+    }
+};
+
+export default Titles;
